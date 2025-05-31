0356782bb2206ebcd03ab2e0d6f7553a99d23cfa
NatchaLatte <foxteary@gmail.com>
2025-06-01 00:56:17 +0700

    add speed-insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import '../app.css';
 	let { children } = $props();
 	injectAnalytics({ mode: dev ? 'development' : 'production' });
+	injectSpeedInsights();
 </script>
 
 {@render children()}
